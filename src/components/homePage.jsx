aa3c27b4971d215d71d3e54cf7f629b4f8cd35f5
kevinseabourne <im_sausaged_as@hotmail.com>
2020-02-17 03:26:05 +0800

    set up homepage component

---
+++ src/components/homePage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const HomePage = props => {
+  return (
+    <Container>
+      <Wrapper></Wrapper>
+    </Container>
+  );
+};
+
+export default HomePage;
+
+const Container = styled.section`
+  width: 100%;
+  height: 2000px;
+  ${"" /* background-color: var(--dark-text-color-soft); */}
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 1300px;
+  margin: 0 auto;
+`;
