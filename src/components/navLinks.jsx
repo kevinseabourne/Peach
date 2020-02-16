bd775aa82263b504de4827ae60662b21f1e41cd3
kevinseabourne <im_sausaged_as@hotmail.com>
2020-02-17 03:26:29 +0800

    seperated header links into their own component

---
+++ src/components/navLinks.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import arrowIcon from "../images/arrow.svg";
+
+const NavLinks = props => {
+  const links = ["Electronics", "Home", "Heath & Fitness", "Travel"];
+  return (
+    <Links>
+      {links.map(link => (
+        <Link key={links.indexOf(link)} icon={arrowIcon}>
+          {link}
+        </Link>
+      ))}
+    </Links>
+  );
+};
+
+export default NavLinks;
+
+const Links = styled.div`
+  margin-left: 220px;
+  margin-right: auto;
+`;
+
+const Link = styled.span`
+  color: var(--light-text-color-strong);
+  font-weight: 600;
+  font-style: normal;
+  margin: 1.3rem;
+  background-size: 4px;
+  transition: 0.2s all ease;
+  &::after {
+    margin: 0 auto;
+    margin-left: 8.8px;
+    content: url(${props => props.icon});
+  }
+  &:hover {
+    cursor: pointer;
+    filter: invert(53%) sepia(92%) saturate(1423%) hue-rotate(183deg)
+      brightness(93%) contrast(88%);
+  }
+`;
