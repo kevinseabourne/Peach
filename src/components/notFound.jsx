742a3434661e42f54b8f3c6b5c0db0ebb8b80781
kevinseabourne <im_sausaged_as@hotmail.com>
2020-02-17 03:23:20 +0800

    second commit

---
+++ src/components/notFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const NotFound = props => {
+  return <Container>404 Page not Found</Container>;
+};
+
+export default NotFound;
+
+const Container = styled.div`
+  font-size: 4px;
+`;
