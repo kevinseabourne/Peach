cc488de06c4a460cca031cc2e08200743cb9d6c1
kevinseabourne <im_sausaged_as@hotmail.com>
2020-02-19 00:12:26 +0800

    newsletter component for sidebar

---
+++ src/components/newsletter.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import Joi from "joi-browser";
+import ReusableForm from "./common/reusableForm";
+
+class Newsletter extends ReusableForm {
+  constructor(props) {
+    super(props);
+    this.emailRef = React.createRef();
+  }
+  state = {
+    data: {
+      email: ""
+    },
+    errors: {}
+  };
+
+  Schema = {
+    email: Joi.string()
+      .required()
+      .email({ minDomainAtoms: 2 })
+      .options({
+        language: {
+          key: "",
+          any: {
+            empty: "Enter a email"
+          },
+          string: {
+            email: "Enter a valid email"
+          }
+        }
+      })
+  };
+
+  doSubmit = async () => {};
+
+  render() {
+    return (
+      <NewsLetterContainer>
+        <SubHeading>NewsLetter</SubHeading>
+        <Message>
+          Subscribe to our newsletter and get our newest updates right on your
+          inbox.
+        </Message>
+        {this.renderInput("email", "Your email address", "", this.emailRef)}
+        {this.renderSubscribeButton("Subscribe", this.state.emailSent)}
+      </NewsLetterContainer>
+    );
+  }
+}
+
+export default Newsletter;
+
+const NewsLetterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 32px;
+`;
+
+const SubHeading = styled.h3`
+  font-size: var(--font-size-lg: 1.125rem);
+`;
+
+const Message = styled.p``;
