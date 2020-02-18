d31bee2871afc244e07ed528fa87e8758d355946
kevinseabourne <im_sausaged_as@hotmail.com>
2020-02-18 23:58:55 +0800

    added tests to header component

--- src/components/header.test.js
+++ src/components/header.test.js
@@ -1,16 +1,50 @@
 import React from "react";
 import Header from "./header";
-import {
-  render,
-  fireEvent,
-  getByTestId,
-  queryByTestId,
-  getByText
-} from "@testing-library/react";
+import { render, fireEvent, act } from "@testing-library/react";
 
 describe("Clicking on BurgerMenu", () => {
-  const { container } = render(<Header />);
-  it("should open a sidebar", () => {
-    expect().toBe();
+  const { getByTestId, queryByTestId } = render(<Header />);
+  it("should open & close the sidebar", () => {
+    // Open
+    const button = getByTestId("burgerMenu");
+    act(() => {
+      fireEvent.click(button);
+    });
+    // Assertion
+    expect(getByTestId("sideBar")).toBeVisible();
+
+    // Close
+    const sideBar = queryByTestId("sideBar");
+    fireEvent.click(button);
+    // Assertion
+    expect(sideBar).not.toBeVisible();
   });
+});
+
+it("should change burgerMenu to false when clicking outside the responsive header", () => {
+  const eventListener = {};
+  window.addEventListener = (evt, cb) => (eventListener[evt] = cb);
+
+  const { getByTestId, queryByTestId } = render(
+    <React.Fragment>
+      <div data-testid="outside" className="outside" />
+      <Header />
+    </React.Fragment>
+  );
+
+  const button = getByTestId("burgerMenu");
+
+  fireEvent.click(button);
+  // Assertion
+  expect(getByTestId("sideBar")).toBeVisible();
+
+  // Simulate click outside
+  act(() => {
+    eventListener.mousedown({
+      target: getByTestId("outside")
+    });
+  });
+  const sideBar = queryByTestId("sideBar");
+  // Assertion
+  expect(sideBar).not.toBeVisible();
 });
