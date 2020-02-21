d580b38243c2138fb8a79e617d4b83c92b825c43
kevinseabourne <im_sausaged_as@hotmail.com>
2020-02-22 00:59:29 +0800

    added search input

--- src/components/headerSearch.jsx
+++ src/components/headerSearch.jsx
@@ -1,13 +1,36 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import styled from "styled-components";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import searchIcon from "../images/search.svg";
+import crossIcon from "../images/cross-icon.svg";
 
 const HeaderSearch = props => {
   const [toggleSearch, setToggleSearch] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (toggleSearch) {
+      ref.current.focus();
+    }
+  }, [toggleSearch]);
 
   const handleSearchClick = () => {
     setToggleSearch(!toggleSearch);
   };
+
+  const handleSearchQuery = value => {
+    setSearchQuery(value);
+  };
+
+  const clearInputOnClick = () => {
+    // decided to use my own clear element in the iput as
+    // there is not support for the html clear element in firefox.
+    setSearchQuery("");
+    // after clearing the input put the input back into focus.
+    ref.current.focus();
+  };
+
   return (
     <Search>
       <SearchIcon
@@ -15,15 +38,45 @@ const HeaderSearch = props => {
         searchIcon={searchIcon}
         data-testid="SearchIcon"
       />
-      <SearchOverlay toggleSearch={toggleSearch} data-testid="SearchOverlay">
-        <ExitButton
-          onClick={handleSearchClick}
-          id="searchExitButton"
-          data-testid="SearchExitButton"
-        >
-          <ExitInner />
-        </ExitButton>
-      </SearchOverlay>
+      <TransitionGroup component={null}>
+        {toggleSearch && (
+          <CSSTransition
+            in={toggleSearch}
+            classNames="searchOverlay"
+            timeout={250}
+          >
+            <SearchOverlay data-testid="SearchOverlay">
+              <ExitButton
+                onClick={handleSearchClick}
+                id="searchExitButton"
+                data-testid="SearchExitButton"
+              >
+                <ExitInner />
+              </ExitButton>
+              <OuterContainer>
+                <SearchBarContainer>
+                  <Input
+                    ref={ref}
+                    type="text"
+                    placeholder="Search..."
+                    value={searchQuery}
+                    onChange={e => handleSearchQuery(e.currentTarget.value)}
+                  />
+                  <IconBox
+                    image={crossIcon}
+                    onClick={clearInputOnClick}
+                    value={searchQuery.length >= 1}
+                    data-testid="searchQueryIconBox"
+                  />
+                </SearchBarContainer>
+                <SearchMessage>
+                  Hit enter to search or ESC to close.
+                </SearchMessage>
+              </OuterContainer>
+            </SearchOverlay>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
     </Search>
   );
 };
@@ -46,23 +99,118 @@ const SearchIcon = styled.div`
 `;
 
 const SearchOverlay = styled.div`
-  visibility: hidden;
   position: fixed;
-  top: -9999;
+  top: 0;
+  left: 0;
   bottom: 0;
-  left: -9999;
   right: 0;
-  opacity: 0.95;
   overflow: hidden;
   transition: all 0.25s;
-  opacity: 0;
+  opacity: 0.95;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: var(--color-black);
-  ${props => !props.toggleSearch} {
-    visibility: visible;
-    top: 0;
-    left: 0;
-    opacity: 1;
+  &.searchOverlay-enter {
+    opacity: 0;
+    transition: all 0.25s;
   }
+  &.searchOverlay-enter-active {
+    opacity: 0.95;
+    transition: all 0.25s;
+  }
+  &.searchOverlay-exit {
+    transition: all 0.25s;
+    opacity: 0.95;
+  }
+  &.searchOverlay-exit-active {
+    opacity: 0;
+    transition: all 0.25s;
+  }
+`;
+
+const SearchBarContainer = styled.div`
+  border-bottom: 3px solid var(--main-background-color);
+  background-color: transparent;
+  height: 64px;
+  width: 704px;
+  margin-top: 6.2px;
+  margin-bottom: 10px;
+  outline: none;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  overflow: hidden;
+  position: relative;
+  transition: all 0.3s ease-in-out;
+  &:focus {
+    border-color: var(--color-accent);
+    transition: all 0.3s ease-in-out;
+  }
+  &:focus-within {
+    font-weight: 500;
+    font-size: 14px;
+    color: rgb(51, 51, 51);
+    border-color: var(--color-accent);
+    outline: none;
+    transition: all 0.3s ease-in-out;
+  }
+`;
+
+const Input = styled.input`
+  padding: "14.5px 40px 0px 12px";
+  border: none;
+  transition: all 0.25s;
+  box-sizing: border-box;
+  background-color: transparent;
+  outline: none;
+  cursor: text;
+  height: 100%;
+  width: 94%;
+  min-height: 42px;
+  font-size: 24px;
+  font-weight: 500;
+  color: var(--main-background-color);
+`;
+
+const IconBox = styled.div`
+  width: 44px;
+  height: 100%;
+  top: 0;
+  bottom: 0;
+  right: 0px;
+  margin: auto;
+  position: absolute;
+  fill: var(--color-accent) !important;
+  border-top-right-radius: 2px;
+  border-bottom-right-radius: 2px;
+  background-color: transparent;
+  background-image: url(${props => props.image});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 31%;
+  transition: all 0.25s;
+  opacity: 0;
+  transform: scale(0);
+  &:hover {
+    cursor: pointer;
+  }
+  ${props => !props.value} {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
+
+const OuterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SearchMessage = styled.span`
+  font-size: 13px;
+  color: var(--main-background-color);
+  display: flex;
+  align-self: flex-end;
 `;
 
 const ExitButton = styled.div`
