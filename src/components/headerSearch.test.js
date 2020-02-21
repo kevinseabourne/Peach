42c90e93cd710b9152e02045e1e9a93812cc1605
kevinseabourne <im_sausaged_as@hotmail.com>
2020-02-22 00:58:43 +0800

    added tests to header search component

--- src/components/headerSearch.test.js
+++ src/components/headerSearch.test.js
@@ -1,22 +1,76 @@
 import React from "react";
 import HeaderSearch from "./headerSearch";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, wait } from "@testing-library/react";
 
 describe("header search component", () => {
-  const { getByTestId } = render(<HeaderSearch />);
   it("should open & close the HeaderSearch component", () => {
+    const { getByTestId, getByPlaceholderText } = render(<HeaderSearch />);
     // Open
     const searchIcon = getByTestId("SearchIcon");
     fireEvent.click(searchIcon);
 
     // Assertion
-    expect(getByTestId("SearchOverlay")).toBeVisible();
+    expect(getByTestId("SearchOverlay")).toBeInTheDocument();
 
     // Close
     const exitButton = getByTestId("SearchExitButton");
     fireEvent.click(exitButton);
 
     // Assertion
-    expect(getByTestId("SearchOverlay")).not.toBeVisible();
+    wait(() => expect(getByTestId("SearchOverlay")).not.toBeInTheDocument());
+  });
+
+  it("should show/hide clear input Button when there is a value or not", () => {
+    const { getByTestId, queryByTestId, getByPlaceholderText } = render(
+      <HeaderSearch />
+    );
+    const searchIcon = getByTestId("SearchIcon");
+    fireEvent.click(searchIcon);
+
+    expect(getByTestId("searchQueryIconBox")).not.toBeVisible();
+    const input = getByPlaceholderText("Search...");
+    fireEvent.change(input, { target: { value: "testing" } });
+
+    const clearInputButton = queryByTestId("searchQueryIconBox");
+    expect(clearInputButton).toBeVisible();
+  });
+
+  it("should clear input value when clicking on clear input Button", () => {
+    const { getByTestId, queryByTestId, getByPlaceholderText } = render(
+      <HeaderSearch />
+    );
+    const searchIcon = getByTestId("SearchIcon");
+    fireEvent.click(searchIcon);
+
+    expect(getByTestId("searchQueryIconBox")).not.toBeVisible();
+    const input = getByPlaceholderText("Search...");
+    fireEvent.change(input, { target: { value: "testing" } });
+
+    const clearInputButton = queryByTestId("searchQueryIconBox");
+    fireEvent.click(clearInputButton);
+    expect(input.value).toBe("");
+  });
+
+  it("should show the correct value on input change", () => {
+    const { getByTestId, queryByTestId, getByPlaceholderText } = render(
+      <HeaderSearch />
+    );
+    const searchIcon = getByTestId("SearchIcon");
+    fireEvent.click(searchIcon);
+
+    const input = getByPlaceholderText("Search...");
+    fireEvent.change(input, { target: { value: "testing" } });
+    expect(input.value).toBe("testing");
+  });
+  it("should focus on the input on mount", () => {
+    const { getByTestId, queryByTestId, getByPlaceholderText } = render(
+      <HeaderSearch />
+    );
+    const searchIcon = getByTestId("SearchIcon");
+    fireEvent.click(searchIcon);
+
+    const input = getByPlaceholderText("Search...");
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveFocus();
   });
 });
