22f19ecd832ea4f3d651819c518325328f1e2fb4
kevinseabourne <im_sausaged_as@hotmail.com>
2020-02-25 23:10:18 +0800

    added test to about component

---
+++ src/components/about.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import About from "./about";
+import { render } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+
+describe("react router", () => {
+  it("render about component", () => {
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <About />
+      </Router>
+    );
+    expect(container).toBeInTheDocument();
+  });
+});
