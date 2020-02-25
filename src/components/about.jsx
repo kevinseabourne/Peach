55e09d436633d22153f4ac36438dd2ff2e598289
kevinseabourne <im_sausaged_as@hotmail.com>
2020-02-25 23:09:46 +0800

    created about component

---
+++ src/components/about.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const About = props => {
+  return (
+    <Container>
+      <Wrapper>about</Wrapper>
+    </Container>
+  );
+};
+
+export default About;
+
+const Container = styled.section`
+  width: 100%;
+  height: 100%;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 1400px;
+`;
