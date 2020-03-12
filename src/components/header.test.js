534fefd711846be50d32657a168ff43b85212365
kevinseabourne <im_sausaged_as@hotmail.com>
2020-03-12 14:51:00 +0800

    added tests and refactored to support react router

--- src/components/header.test.js
+++ src/components/header.test.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Header from "./header";
+import BottomHeader from "./bottomHeader";
 import { render, fireEvent, act } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
 describe("Clicking on BurgerMenu", () => {
-  const { getByTestId, queryByTestId } = render(<Header />);
+  const history = createMemoryHistory();
+  const { getByTestId, queryByTestId } = render(
+    <Router history={history}>
+      <Header />
+    </Router>
+  );
   it("should open & close the sidebar", () => {
     // Open
     const button = getByTestId("burgerMenu");
@@ -19,32 +27,67 @@ describe("Clicking on BurgerMenu", () => {
     // Assertion
     expect(sideBar).not.toBeVisible();
   });
-});
 
-it("should change burgerMenu to false when clicking outside the responsive header", () => {
-  const eventListener = {};
-  window.addEventListener = (evt, cb) => (eventListener[evt] = cb);
+  it("should not close the sideBar when clicking on the sideBar", () => {
+    const eventListener = {};
+    window.addEventListener = (evt, cb) => (eventListener[evt] = cb);
 
-  const { getByTestId, queryByTestId } = render(
-    <React.Fragment>
-      <div data-testid="outside" className="outside" />
-      <Header />
-    </React.Fragment>
-  );
+    const history = createMemoryHistory();
+    const { getByTestId, queryByTestId } = render(
+      <React.Fragment>
+        <div data-testid="outside" className="outside" />
+        <Router history={history}>
+          <Header />
+        </Router>
+      </React.Fragment>
+    );
 
-  const button = getByTestId("burgerMenu");
+    const button = getByTestId("burgerMenu");
 
-  fireEvent.click(button);
-  // Assertion
-  expect(getByTestId("sideBar")).toBeVisible();
+    fireEvent.click(button);
+    // Assertion
+    expect(getByTestId("sideBar")).toBeVisible();
 
-  // Simulate click outside
-  act(() => {
-    eventListener.mousedown({
-      target: getByTestId("outside")
+    // Simulate click outside
+    act(() => {
+      eventListener.mousedown({
+        target: getByTestId("sideBar")
+      });
     });
+
+    const sideBar = queryByTestId("sideBar");
+    // Assertion
+    expect(sideBar).toBeVisible();
   });
-  const sideBar = queryByTestId("sideBar");
-  // Assertion
-  expect(sideBar).not.toBeVisible();
+
+  it("should change burgerMenu to false when clicking outside the responsive header", () => {
+    const eventListener = {};
+    window.addEventListener = (evt, cb) => (eventListener[evt] = cb);
+
+    const history = createMemoryHistory();
+    const { getByTestId, queryByTestId } = render(
+      <React.Fragment>
+        <div data-testid="outside" className="outside" />
+        <Router history={history}>
+          <Header />
+        </Router>
+      </React.Fragment>
+    );
+
+    const button = getByTestId("burgerMenu");
+
+    fireEvent.click(button);
+    // Assertion
+    expect(getByTestId("sideBar")).toBeVisible();
+
+    // Simulate click outside
+    act(() => {
+      eventListener.mousedown({
+        target: getByTestId("outside")
+      });
+    });
+    const sideBar = queryByTestId("sideBar");
+    // Assertion
+    expect(sideBar).not.toBeVisible();
+  });
 });
