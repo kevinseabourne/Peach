8407a22f999de4df39e96a0cbd32aa7e1ccf1c1d
kevinseabourne <im_sausaged_as@hotmail.com>
2020-03-12 14:48:27 +0800

    refactored test to support react router

--- src/components/sideBar.test.js
+++ src/components/sideBar.test.js
@@ -1,9 +1,16 @@
 import React from "react";
 import SideBar from "./sideBar";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 import { render, fireEvent } from "@testing-library/react";
 
 describe("Clicking on ExitButton in sideBar", () => {
-  const { getByTestId, queryByTestId } = render(<SideBar />);
+  const history = createMemoryHistory();
+  const { getByTestId, queryByTestId } = render(
+    <Router history={history}>
+      <SideBar />
+    </Router>
+  );
   it("should close the sidebar", () => {
     const button = getByTestId("sideBarExitButton");
     fireEvent.click(button);
