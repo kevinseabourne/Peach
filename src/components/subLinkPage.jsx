c8f2eeb59505cac02efee6021267c545fcbf2885
kevinseabourne <im_sausaged_as@hotmail.com>
2020-03-12 14:47:29 +0800

    created SubLink Page

---
+++ src/components/subLinkPage.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { useLocation, useParams } from "react-router-dom";
+
+const SubLinkPage = props => {
+  const location = useLocation();
+
+  const subLinkPageDataLS = () =>
+    JSON.parse(localStorage.getItem("subLinkPageData")) || {};
+  const [subLinkPageData, setSubLinkPageData] = useState(subLinkPageDataLS);
+
+  useEffect(() => {
+    if (location.state) {
+      localStorage.setItem("subLinkPageData", JSON.stringify(location.state));
+      setSubLinkPageData(location.state);
+    }
+  }, [location.state]);
+
+  return (
+    <Container data-testid={`subLink${subLinkPageData.subLink}Page`}>
+      <Wrapper>{subLinkPageData.subLink}</Wrapper>
+    </Container>
+  );
+};
+
+export default SubLinkPage;
+
+const Container = styled.section`
+  width: 100%;
+  height: 2000px;
+  ${"" /* background-color: var(--dark-text-color-soft); */}
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  max-width: 1300px;
+  margin: 0 auto;
+`;
