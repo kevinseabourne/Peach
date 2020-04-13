86539f7a9a0071974988f16426be17c9bd8ae276
kevinseabourne <im_sausaged_as@hotmail.com>
2020-04-13 19:23:59 +0800

    moved some functions to featuredSection component

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
-import SliderSection from "./sliderSection";
+import FeaturedSection from "./featuredSection";
 import { getAllArticles } from "../components/services/articleService";
 import _ from "lodash";
 
@@ -8,6 +8,7 @@ const HomePage = props => {
   const [allArticles, setAllArticles] = useState([]);
   const [featuredArticles, setFeaturedArticles] = useState([]);
   // const [popularArticles, setPopularArticles] = useState([]);
+
   useEffect(() => {
     async function fetchData() {
       const { data } = await getAllArticles();
@@ -16,93 +17,17 @@ const HomePage = props => {
       // and admin picks for the carousel along with editor picks at the bottom of the homepage.
       // console.log(allArticles);
       setAllArticles(data);
-      handleFeaturedArticles(data);
     }
     fetchData();
+    return () => {
+      // timer.current && clearTimeout(timer.current);
+    };
   }, []);
-
-  const handleFeaturedArticles = articles => {
-    // In the admin page, give the ability to set which articles are featured articles max 4 articles.
-    const featuredArticles = articles.filter(
-      article => article.featuredArticle
-    );
-    // giving each article an animation direction
-    featuredArticles.map(article => {
-      if (featuredArticles.indexOf(article) === 0) {
-        article.animationDirection = "left";
-        article.selected = true;
-        return article;
-      } else {
-        article.animationDirection = "right";
-        article.selected = false;
-        return article;
-      }
-    });
-    setFeaturedArticles(featuredArticles);
-  };
-
-  const handleFeaturedArticleChange = SelectedArticle => {
-    const featuredArticlesClone = _.clone(featuredArticles);
-    const currentArticleIndex = featuredArticlesClone.findIndex(
-      article => article.selected
-    );
-    const selectedArticleIndex = featuredArticlesClone.indexOf(SelectedArticle);
-
-    const updatedFeaturedArticles = featuredArticlesClone.map(article => {
-      if (SelectedArticle.id === article.id) {
-        article.selected = true;
-        if (selectedArticleIndex > currentArticleIndex) {
-          article.animationDirection = "right";
-        } else {
-          article.animationDirection = "left";
-        }
-        return article;
-      } else {
-        article.selected = false;
-        return article;
-      }
-    });
-    setFeaturedArticles(updatedFeaturedArticles);
-  };
-
-  const onImageLoad = loadedArticle => {
-    console.log("we in");
-    const featuredArticlesClone = _.clone(featuredArticles);
-
-    const updatedFeaturedArticles = featuredArticlesClone.map(article => {
-      article.imageLoaded = true;
-      return article;
-    });
-    setFeaturedArticles(updatedFeaturedArticles);
-    // const { featuredArticles } = props;
-    // if (loadedArticle.imageLoaded) {
-    //   const featuredArticlesClone = _.clone(featuredArticles);
-    //
-    //   featuredArticlesClone.map(article => {
-    //     article.imageLoaded = false;
-    //     return article;
-    //   });
-    //   setFeaturedArticles(featuredArticlesClone);
-    // } else {
-    //   const featuredArticlesClone = _.clone(featuredArticles);
-    //
-    //   featuredArticlesClone.map(article => {
-    //     return loadedArticle.id === article.id && article.image
-    //       ? (article.imageLoaded = true)
-    //       : (article.imageLoaded = false);
-    //   });
-    //   setFeaturedArticles(featuredArticlesClone);
-    // }
-  };
 
   return (
     <Container data-testid="homePage">
       <Wrapper>
-        <SliderSection
-          featuredArticles={featuredArticles}
-          handleFeaturedArticleChange={handleFeaturedArticleChange}
-          onImageLoad={onImageLoad}
-        />
+        <FeaturedSection />
       </Wrapper>
     </Container>
   );
