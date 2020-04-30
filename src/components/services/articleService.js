dca6a4c3317027fcd4dfa896934377a615683aa6
kevinseabourne <im_sausaged_as@hotmail.com>
2020-04-30 19:33:13 +0800

    added CancelToken to axios http request

--- src/components/services/articleService.js
+++ src/components/services/articleService.js
@@ -1,13 +1,19 @@
-import http from "./httpService";
+import axios from "axios";
 
 const apiEndPoint = "http://localhost:3000/api/articles";
 
-export async function getAllArticles() {
-  let data = await http.get(apiEndPoint);
-  return data;
-}
+const CancelToken = axios.CancelToken;
+let source = CancelToken.source();
 
-export async function getArticle(id) {
-  let data = await http.get(`${apiEndPoint}/${id}`);
-  return data;
+export async function getAllArticles(request) {
+  if (request === undefined) {
+    source = axios.CancelToken.source();
+  }
+  if (request === "cancel") {
+    source.cancel();
+  }
+  const response = await axios.get(apiEndPoint, {
+    cancelToken: source.token
+  });
+  return response;
 }
