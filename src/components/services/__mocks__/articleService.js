b3a0cd4dca50e3e75460d3d76dd2ede0c0f90284
kevinseabourne <im_sausaged_as@hotmail.com>
2020-04-30 19:33:38 +0800

    mock

---
+++ src/components/services/__mocks__/articleService.js
@@ -0,0 +1,39 @@
+export const data = [
+  {
+    category: "Routers Mock",
+    section: "Electronics",
+    author: "Mock Data",
+    datePublished: "2020-04-06T03:30:23.906Z",
+    dateModified: "2020-04-06T03:30:23.906Z",
+    views: 0,
+    featuredArticle: true,
+    image: [
+      "https://chpistel.sirv.com/Peach/webaroo-tN344soypQM-unsplash.jpg?q=1",
+      "https://chpistel.sirv.com/Peach/webaroo-tN344soypQM-unsplash.jpg"
+    ],
+    title: "Top 5 Best Routers 2020 Mock",
+    description:
+      "Choice from the best routers for gaming entertainment, and cheap with this top 5 list.",
+    products: [{}],
+    id: "5e8ac659315846be2d708ebd"
+  },
+  {
+    category: "VPNs Mock",
+    section: "Electronics",
+    author: "Mock Data",
+    datePublished: "2020-05-06T03:30:23.906Z",
+    dateModified: "2020-05-06T03:30:23.906Z",
+    views: 0,
+    featuredArticle: true,
+    image: [
+      "https://chpistel.sirv.com/Peach/webaroo-tN344soypQM-unsplash.jpg?q=1",
+      "https://chpistel.sirv.com/Peach/webaroo-tN344soypQM-unsplash.jpg"
+    ],
+    title: "Top 5 Best VPNs 2020 Mock",
+    description: "Choice from the best VPNs",
+    products: [{}],
+    id: "5e8ac659315846be2d708eda"
+  }
+];
+
+export const getAllArticles = jest.fn();
